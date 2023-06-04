00af425e6d9e309f25e7e96a556f50cc7774fb9c
tianranan <tayrsi@outlook.com>
2023-06-04 23:21:29 +0800

    添加viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8888,
+    open: true,
+  }
+  // preview:{
+  //   port: 8000,
+  //   open: true,
+  // }
 })

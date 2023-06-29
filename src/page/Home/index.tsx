ef2bdb89f3a386e51bf45eb2075ff648583fd09b
EuBain <tayrsi@outlook.com>
2023-06-30 02:01:49 +0800

    函数问题

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,15 +1,37 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import * as rr from 'react-router-dom'
 
 const Home:FC = () => {
   const navigate = rr.useNavigate()
 
-  const a = {foo: '232' ,bar: {biz: '1'}}
-  const b = a 
-  b.bar.ccc = [2]
-  console.log(Object.is(b,a),'a',a,'b',b)
+  
+  function a () {
+    b()
+    console.log(3)
+    return;
+}
+
+async  function b() {
+let i = 10
+     while(i > 0) {
+        i -= 1
+        const ff = await 
+        new Promise((resolve) => {
+            setTimeout(() => resolve('2'), 2000)
+        })
+        console.log(ff) 
+    }
+}
+useEffect(()=>{
+return ()=> {
+  console.log('销毁了')
+}
+}, [])
   return (
-    <div>Home</div>
+  <>
+    <div onClick={() => navigate('/mark')}>Home</div>
+    <input type="file" onChange={a}/>
+  </>
   )
 }
 
--- src/page/Mark/index.tsx
+++ src/page/Mark/index.tsx
@@ -1,7 +1,8 @@
 import { Tabs, TabsProps } from 'antd';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { markList } from './constant';
 import MarkDownInHTML from '@/components/MarkDownInHTML';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const items: TabsProps['items'] =markList.map((item,index) => ({
     key: (index+1).toString(),
@@ -14,10 +15,35 @@ const Mark = () => {
     const onChange = (key: string) => {
         console.log(key);
       };
-      
+      const navigate = useNavigate()
 
+  
+      function a () {
+        b()
+        console.log(3)
+        return;
+    }
+    
+    async  function b() {
+    let i = 10
+         while(i > 0) {
+            i -= 1
+            const ff = await 
+            new Promise((resolve) => {
+                setTimeout(() => resolve('2'), 2000)
+            })
+            console.log(ff) 
+        }
+    }
+    useEffect(()=>{
+    return ()=> {
+      console.log('销毁了')
+    }
+  })
   return (   
     <div>
+          <div onClick={() => navigate('/home')}>mark</div>
+    <input type="file" onChange={a}/>
         <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
     </div>
   )
